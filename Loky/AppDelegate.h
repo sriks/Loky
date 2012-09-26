933ca9824a2364d65db6743fca3c2e6352fe8ca3
Srikanth Sombhatla <srikanthsombhatla@gmail.com>
2012-09-26 21:16:19 +0530

    Initial Commit

---
+++ Loky/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Loky
+//
+//  Created by Srikanth Sombhatla on 26/09/12.
+//  Copyright (c) 2012 Kony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Loky/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Loky
+//
+//  Created by Srikanth Sombhatla on 26/09/12.
+//  Copyright (c) 2012 Kony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
